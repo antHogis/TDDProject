3c1afa3179551f42f7c26c927fe35685ba56e80a
antHogis <antonjhoglund@hotmail.com>
2018-11-28 20:54:37 +0200

    Update name setters in Person

--- TddProject/src/main/java/com/github/anthogis/tddproject/Person.java
+++ TddProject/src/main/java/com/github/anthogis/tddproject/Person.java
@@ -10,10 +10,13 @@ public class Person {
     private LocalDate birthDate;
 
      public Person() {
-
+         firstName = null;
+         lastName = null;
+         birthDate = null;
      }
 
-    public Person(String firstName, String lastName, LocalDate birthDate) {
+    public Person(String firstName, String lastName, LocalDate birthDate) 
+            throws PersonNotBornYetException, GetANewNameException {
         setFirstName(firstName);
         setLastName(lastName);
         setBirthDate(birthDate);
@@ -23,16 +26,24 @@ public class Person {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFirstName(String firstName) throws GetANewNameException {
+        if (firstName.equals(null) || firstName.length() < 2) {
+            throw new GetANewNameException("Get a new name, you freak");
+        } else {
+            this.firstName = firstName;
+        }
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setLastName(String lastName) throws GetANewNameException {
+        if (lastName.equals(null) || lastName.length() < 2) {
+            throw new GetANewNameException("Get a new name, you freak");            
+        } else {
+            this.lastName = lastName;
+        }
     }
 
     public LocalDate getBirthDate() {
@@ -55,6 +66,24 @@ public class Person {
     public class PersonNotBornYetException extends RuntimeException{
         public PersonNotBornYetException(String message) {
             super(message);
+        }
+    }
+    
+    public class GetANewNameException extends RuntimeException {
+         public GetANewNameException(String message) {
+             super(message);
+         }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Person) {
+            return this.firstName.equals(((Person) obj).getFirstName()) &&
+                    this.lastName.equals(((Person) obj).getLastName()) &&
+                    this.birthDate.equals(((Person) obj).getBirthDate());
+            
+        } else {
+            return false;
         }
     }
 }
