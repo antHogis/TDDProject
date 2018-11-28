b5c3dce72329552957c45444ddd66f435fc866fc
antHogis <antonjhoglund@hotmail.com>
2018-11-28 17:46:02 +0200

    Add test class TestPerson

--- TddProject/src/test/java/TddProjectSuite.java
+++ TddProject/src/test/java/TddProjectSuite.java
@@ -3,7 +3,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-
+    TestPerson.class
 })
 public class TddProjectSuite {
+    //This class only functions as a holder for annotations above.
 }
---
+++ TddProject/src/test/java/TestPerson.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestPerson {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
