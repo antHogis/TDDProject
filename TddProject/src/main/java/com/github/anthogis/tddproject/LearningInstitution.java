486a29174979dff96f3cfc3a7dbb13e6c042b75e
antHogis <antonjhoglund@hotmail.com>
2018-11-28 21:50:23 +0200

    Update LearningInstitution and test

--- TddProject/src/main/java/com/github/anthogis/tddproject/LearningInstitution.java
+++ TddProject/src/main/java/com/github/anthogis/tddproject/LearningInstitution.java
@@ -1,8 +1,25 @@
 package com.github.anthogis.tddproject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LearningInstitution {
     private List<Person> students;
+    private List<Course> courses;
+    private int grantMoney;
+
+    public LearningInstitution() {
+        students = new ArrayList<>();
+        courses = new ArrayList<>();
+        grantMoney = 0;
+    }
+
+    public void addGrant(int amount) {
+        grantMoney += amount;
+    }
+
+    public int getGrantMoney() {
+        return grantMoney;
+    }
 
 }
--- TddProject/src/test/java/TestLearningInstitution.java
+++ TddProject/src/test/java/TestLearningInstitution.java
@@ -1,2 +1,22 @@
+import com.github.anthogis.tddproject.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TestLearningInstitution {
+    static LearningInstitution school;
+
+    @BeforeClass
+    public static void init() {
+        school = new LearningInstitution();
+    }
+
+    @Test
+    public void testAddGrant() {
+        int addAmount = 10;
+        int initialAmount = school.getGrantMoney();
+        school.addGrant(addAmount);
+        assertTrue(school.getGrantMoney() == initialAmount + addAmount);
+    }
 }
