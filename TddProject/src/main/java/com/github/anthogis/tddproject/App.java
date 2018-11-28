21a36cc68194a58b632ad88b58bc82cb6551ef1b
antHogis <antonjhoglund@hotmail.com>
2018-11-28 22:39:33 +0200

    Rework App command execution

--- TddProject/src/main/java/com/github/anthogis/tddproject/App.java
+++ TddProject/src/main/java/com/github/anthogis/tddproject/App.java
@@ -3,45 +3,61 @@ package com.github.anthogis.tddproject;
 import java.util.Scanner;
 
 public class App {
+    static LearningInstitution school;
+    static boolean run;
+
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean run = true;
-        LearningInstitution school = new LearningInstitution();
+        run = true;
+        school = new LearningInstitution();
 
-        System.out.println("Welcome!\nType help to see list of commands");
+        System.out.println("Welcome!\nType help to see list of commands.");
 
         while (run) {
             System.out.print('>');
-            run = handleInput(scanner.nextLine());
+            handleInput(scanner.nextLine());
         }
     }
 
-    private static boolean handleInput(String input) {
-        boolean run = true;
-
-        if (input.equals(Commands.HELP.command)) {
-            for (Commands c : Commands.values()) {
-                System.out.printf("%s - %s\n", c.command, c.description);
+    private static void handleInput(String input) {
+        boolean validCommand = false;
+        for (Commands c : Commands.values()) {
+            if (input.equals(c.command)) {
+                c.runnable.run();
+                validCommand = true;
             }
-        } else if (input.equals(Commands.QUIT.command)) {
-            System.out.println("Goodbye!");
-            run = false;
         }
 
-        return run;
+        if (!validCommand) System.out.println("Invalid input!");
     }
 
     private enum Commands {
-        HELP("help", "List all commands"),
-        QUIT("quit", "Quit the app."),
-        LIST("add student", "List fucks given");
+        HELP("help", "List all commands", Commands::help),
+        QUIT("quit", "Quit the app.", Commands::quit),
+        ADD_STUDENT("add student", "Add student to the school", Commands::addStudent);
 
         private String command;
         private String description;
+        private Runnable runnable;
 
-        Commands(String command, String description) {
+        Commands(String command, String description, Runnable runnable) {
             this.command = command;
             this.description = description;
+            this.runnable = runnable;
+        }
+
+        static void help() {
+            for (Commands c : Commands.values()) {
+                System.out.printf("%s - %s\n", c.command, c.description);
+            }
+        }
+
+        static void quit() {
+            App.run = false;
+        }
+
+        static void addStudent() {
+
         }
     }
 }
