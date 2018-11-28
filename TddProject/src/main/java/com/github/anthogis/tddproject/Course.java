babf410348f33ea24c6f6e21d8c734561d318712
antHogis <antonjhoglund@hotmail.com>
2018-11-28 21:30:55 +0200

    Add method addStudent to Course and test

--- TddProject/src/main/java/com/github/anthogis/tddproject/Course.java
+++ TddProject/src/main/java/com/github/anthogis/tddproject/Course.java
@@ -1,4 +1,27 @@
 package com.github.anthogis.tddproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
+    private int MAX_STUDENTS;
+    private List<Person> students;
+
+    public Course(int MAX_STUDENTS) throws IllegalArgumentException{
+        setMAX_STUDENTS(MAX_STUDENTS);
+        students = new ArrayList<>(MAX_STUDENTS);
+    }
+
+    public boolean addStudent(Person student) {
+        //If capacity not full, add Person and return true if successful
+        return students.size() < MAX_STUDENTS ? students.add(student) : false;
+    }
+
+    public void setMAX_STUDENTS(int MAX_STUDENTS) throws IllegalArgumentException {
+        if (MAX_STUDENTS > 0) {
+            this.MAX_STUDENTS = MAX_STUDENTS;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 }
--- TddProject/src/test/java/TestCourse.java
+++ TddProject/src/test/java/TestCourse.java
@@ -1,2 +1,15 @@
+import com.github.anthogis.tddproject.Course;
+import com.github.anthogis.tddproject.Person;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TestCourse {
+
+    @Test
+    public void testAdd() {
+        Course course = new Course(1);
+        course.addStudent(new Person());
+        assertFalse(course.addStudent(new Person()));
+    }
 }
