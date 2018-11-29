bbb6206ccc604fb3b931ebe10116a7f28496011d
antHogis <antonjhoglund@hotmail.com>
2018-11-29 12:55:00 +0200

    Test adding courses to LearningInstitution

--- TddProject/src/test/java/TestLearningInstitution.java
+++ TddProject/src/test/java/TestLearningInstitution.java
@@ -2,14 +2,20 @@ import com.github.anthogis.tddproject.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.Assert.*;
 
 public class TestLearningInstitution {
     static LearningInstitution school;
+    static Person person;
+    static Course course;
 
     @BeforeClass
     public static void init() {
         school = new LearningInstitution();
+        person = new Person("Testy", "Testington", LocalDate.now());
+        course = new Course(1, "Test Course");
     }
 
     @Test
@@ -31,16 +37,40 @@ public class TestLearningInstitution {
     }
 
     @Test
+    public void testAddStudent() {
+        init();
+        school.addStudent(person);
+        assertEquals(1, school.studentsAmount());
+        assertEquals(10, school.getGrantMoney());
+    }
+
+    @Test (expected = InsufficientFundsException.class)
+    public void testAddCourseInsufficientFunds() {
+        init();
+        school.addCourse(course);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testAddCourseIllegalArgument() {
+        init();
+        school.addStudent(person);
+        school.addStudent(person);
+        school.addCourse(new Course(-1, "Test"));
+    }
+
+    @Test
+    public void testAddCourseValid() {
+        init();
+        school.addStudent(person);
+        school.addStudent(person);
+        school.addCourse(course);
+    }
+
+    @Test
     public void getGrantMoney() {
     }
 
-    @Test
-    public void addStudent() {
-    }
 
-    @Test
-    public void addCourse() {
-    }
 
     @Test
     public void getStudentInfoList() {
